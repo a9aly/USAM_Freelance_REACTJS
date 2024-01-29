36c3e805f161b937fbcc4e019e622283ea2f536b
Ahmed <ahmedali9@aucegypt.edu>
2024-01-29 04:47:56 +0200

    add: Home Page v1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,6 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import { postJob } from "../../redux/store/PostSlice";
 const Home = () => {
   const data = useSelector((state) => state.post);
   const [activeCategory, setActiveCategory] = useState("product management");
@@ -16,9 +17,11 @@ const Home = () => {
     no_jobs: "",
   };
   const [jobPost, setJobPost] = useState(jobTemplate);
+  const dispatch = useDispatch();
+
   const handlePostEvent = (e) => {
     e.preventDefault();
-    console.log(jobPost);
+    dispatch(postJob(jobPost));
   };
 
   return (
@@ -87,7 +90,7 @@ const Home = () => {
             setJobPost({
               ...jobPost,
               company: e.target.value,
-              logo: e.target.value + ".png",
+              logo: e.target.value.toLowerCase() + ".png",
             })
           }
         />
@@ -224,13 +227,13 @@ const Home = () => {
           {/* view job */}
           <div className="job-container flex-grow ">
             {data.jobs.map((job) => {
-              if (activeCategory === job.category)
+              if (activeCategory.toLowerCase() === job.category)
                 return (
                   <div className="job-card" key={job.id}>
                     <span className="block text-2xl font-bold mb-[5%] capitalize">
                       {job.job_title}
                     </span>
-                    <div className="flex flex-row w-full flex-wrap lg:flex-nowrap">
+                    <div className="flex flex-row w-full flex-wrap">
                       <span className="block capitalize border-2 bg-button-color bg-opacity-30 rounded-full  p-[4%] m-[3%] whitespace-nowrap">
                         {job.type}
                       </span>
@@ -259,7 +262,7 @@ const Home = () => {
                             src="users.png"
                             className=" w-[15%] inline mr-1 "
                           />
-                          {job.no_applicants}
+                          140-300
                         </span>
                       </span>
                       <span className="border-2 bg-jobs-number-color w-fit h-fit rounded-full p-[2%] capitalize whitespace-nowrap">
@@ -274,11 +277,152 @@ const Home = () => {
       </div>
 
       {/* Talent Section */}
-      <div className="wrapper bg-pri-color">
+      <div className="wrapper bg-pri-color h-fit">
         <h1>
           <span className=" text-button-color">Browse </span>Talent By Category
         </h1>
+        <div className="talent-container pb-[150px]">
+          {data.talent_category.map((talent) => (
+            <div className="talent-card" key={talent.name}>
+              <img src="gitlab.png" />
+              <h1>
+                {talent.name}
+                <br />
+                <div className=" font-extralight text-sm flex justify-between flex-grow">
+                  <div>
+                    <img
+                      src="greenstar.png"
+                      alt="rating-star"
+                      className="!w-4"
+                    />
+                    <span> &nbsp; 4.85/5 </span>
+                    <span>349 skills</span>
+                  </div>
+                </div>
+              </h1>
+            </div>
+          ))}
+          <div className="flex flex-row align-center justify-center w-[300%]">
+            <div className=" border text-center  w-fit h-fit bg-button-color text-white p-5">
+              More Categories
+            </div>
+          </div>
+        </div>
       </div>
+
+      {/*Portofilos */}
+      <div className="wrapper flex flex-col w-full text-center ">
+        <h2 className=" font-thin mb-0">
+          Logos, Websites, book covers & more!
+        </h2>
+        <h1 className=" mt-2">
+          Checkout The Best
+          <span className=" text-button-color"> Portofilos</span> Here
+        </h1>
+        <div className="grid grid-flow-col">
+          <div className="portofolio-card">
+            <img src="person.png" />
+            <h2 className=" text-left pl-5 ">Jenny Wilson</h2>
+            <h3 className="text-left pl-5 font-thin">UI/UX Designer</h3>
+          </div>
+          <div className="portofolio-card">
+            <img src="Person2.png" />
+            <h2 className=" text-left pl-5">Dianne Russell</h2>
+            <h3 className="text-left pl-5 font-thin">Graphic Designer</h3>
+          </div>
+          <div className="portofolio-card">
+            <img src="Person3.png" />
+            <h2 className=" text-left pl-5">Albert Flores</h2>
+            <h3 className="text-left pl-5 font-thin">Developer</h3>
+          </div>
+        </div>
+      </div>
+
+      {/*Subscription */}
+      <div className="wrapper flex flex-col w-full  items-center bg-pri-color pb-10">
+        <h1>Newsletter Subscription</h1>
+        <h3 className=" font-extralight mt-0">
+          Subscribe to our newsletter to get new freelance work and projects
+        </h3>
+        <input
+          className=" w-[30%] mb-5"
+          placeholder="Enter your email address"
+        ></input>
+        <button className=" bg-button-color text-white min-w-min">
+          Subscribe
+        </button>
+      </div>
+
+      {/*footer*/}
+      <div className="wrapper flex flex-row p-5 justify-evenly ">
+        <div className="w-[15%]">
+          <img src="usam.png" alt="logo" />
+          <h4 className=" text-button-color">
+            Powerful Freelance Marketplace System with ability to change the
+            Users (Freelance & Clients)
+          </h4>
+          <span className="flex flex-row gap-4">
+            <img
+              src="instagram.svg"
+              alt="instagram logo"
+              className=" w-6 h-6"
+            />
+            <img src="twitter.svg" alt="twitter logo" className=" w-6 h-6" />
+            <img src="facebook.png" alt="facebook logo" className=" w-6 h-6" />
+          </span>
+        </div>
+        <div>
+          <h3 className=" font-bold">For Clients</h3>
+          <ul>
+            <li>Find Freelancers</li>
+            <li>Post Project</li>
+            <li>Refund Policy</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="font-bold">For Freelancers</h3>
+          <ul>
+            <li>Find Work</li>
+            <li>Create Account</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="font-bold">Call Us</h3>
+          <ul>
+            <li>
+              {" "}
+              <img
+                src="location.svg"
+                alt="location icon"
+                className=" w-4 h-4 inline-block mr-2"
+              />
+              Egypt
+            </li>
+            <li>
+              {" "}
+              <img
+                src="phone.svg"
+                alt="phone icon"
+                className=" w-4 h-4 inline-block mr-2"
+              />
+              +0254700000000
+            </li>
+            <li>
+              {" "}
+              <img
+                src="email.svg"
+                alt="email icon"
+                className=" w-4 h-4 inline-block mr-2"
+              />
+              USAM@gmail.com
+            </li>
+          </ul>
+        </div>
+      </div>
+      <span className="wrapper text-center font-extralight mb-10">
+        2024 USAM. All right reserved
+      </span>
     </div>
   );
 };
--- src/redux/store/PostSlice.jsx
+++ src/redux/store/PostSlice.jsx
@@ -1,10 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import * as data from "../../db.json";
+import axios from "axios";
+
+export const postJob = createAsyncThunk("postJob", async (postContent) => {
+  const request = await axios.post("http://localhost:3000/job", postContent);
+  const response = await request.data.data;
+  return response;
+});
+
 const postSlice = createSlice({
   name: "post",
   initialState: {
     categories: data.categories,
     jobs: data.job,
+    talent_category: data.talent_category,
     no_project_management: 0,
     no_design: 0,
     no_development: 0,
